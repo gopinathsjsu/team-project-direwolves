4b006fae6d3e7fea98d0041ab1e397f04905a9d8
Divya Mittal <divyacs1991@gmail.com>
2021-11-23 00:12:15 -0800

    Base for backend and mongo models

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:"mongodb+srv://vinay:vinayguda@cluster0.fjuaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secret: "cmpe202_secret_key",
+    frontEnd: "http://localhost:3000"
+}
