ca0e1f5f5eb3625a17936bc65aa413f3ff543cac
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-11-25 01:30:00 -0800

    Added html bookings

---
+++ frontend/src/Components/Flights/BookingList.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+const BookingList = () => {
+  return (
+    <tbody class="atm-c-table__body app-containers-ManageResMyTrips-panels-styles__panel--2gGlj">
+      <tr class="atm-c-table__row">
+        <td class="atm-c-table__cell atm-c-table__cell--align-left">
+          <div class="app-containers-ManageResMyTrips-panels-styles__pastTripColumn--1QPi4">
+            <p class="app-containers-ManageResMyTrips-panels-styles__tripTo--txTbK">
+              San Francisco
+            </p>
+            <p class="app-containers-ManageResMyTrips-panels-styles__pastConfirmationNumber--3E7wb">
+              D1N574
+            </p>
+          </div>
+        </td>
+        <td class="atm-c-table__cell atm-c-table__cell--align-left" colspan="2">
+          <div class="app-containers-ManageResMyTrips-panels-styles__dateFlightItem--1ZURK">
+            <div class="atm-c-text-passage app-containers-ManageResMyTrips-panels-styles__date--27yJ6">
+              <div class="atm-l-linelength-container">
+                <span>Mon, Aug 9, 2021</span>
+              </div>
+            </div>
+            <ul class="app-containers-ManageResMyTrips-panels-styles__flights--1YRWB">
+              <li>BOM - DEL</li>
+            </ul>
+          </div>
+          <div class="app-containers-ManageResMyTrips-panels-styles__dateFlightItem--1ZURK">
+            <div class="atm-c-text-passage app-containers-ManageResMyTrips-panels-styles__date--27yJ6">
+              <div class="atm-l-linelength-container">
+                <span>Tue, Aug 10, 2021</span>
+              </div>
+            </div>
+            <ul class="app-containers-ManageResMyTrips-panels-styles__flights--1YRWB">
+              <li>DEL - SFO</li>
+            </ul>
+          </div>
+        </td>
+        <td
+          class="atm-c-table__cell atm-c-table__cell--align-left"
+          data-heading="Traveler"
+        >
+          <ul>
+            <li>Rugved Manoorkar</li>
+          </ul>
+        </td>
+        <td
+          class="atm-c-table__cell app-containers-ManageResMyTrips-panels-styles__receiptLinks--1ZQxp atm-c-table__cell--align-left"
+          data-heading="Receipt"
+        >
+          <button class="atm-c-btn atm-c-btn--link">
+            <span class="atm-c-btn__text"></span>
+          </button>
+          <br />
+          <a
+            class="app-containers-ManageResMyTrips-panels-styles__receiptLink--3low8"
+            href="/en/us/receipts"
+          >
+            Find all receipts
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default BookingList;
---
+++ frontend/src/Components/Flights/Bookings.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Bookings = () => {
+  return (
+    <div className="past-flights">
+      <div class="transform endow rectangle">
+        <div class="swap" style="">
+          <div class="transform endow rectangle card card--full my-account-past-flights-card">
+            <div class="swap">
+              <div class="header-bar">
+                <div class="transform endow rectangle header-bar--container">
+                  <div class="endow--backdrop-outer-container">
+                    <div class="endow--backdrop-background swa-g-color-bg-neutral-gray2"></div>
+                  </div>
+                  <div class="swap header-bar--content">
+                    <div class="header-bar--content">
+                      <h2
+                        class="heading heading_semi-large header-bar--heading header-bar--heading_focusable"
+                        id="past"
+                        tabindex="-1"
+                      >
+                        0 past flights
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="transform endow rectangle">
+                <div class="swap">
+                  <div class="my-account-past-flights-card--filter-area"></div>
+                  <div class="past-flights-card--no-results-section">
+                    <div class="my-account-no-results-content">
+                      <span class="my-account-no-results-content--title">
+                        You have no past flights.
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bookings;
