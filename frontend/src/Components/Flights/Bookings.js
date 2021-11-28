b93d7be30646f05762cd6527dba59a92c2b50255
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-11-28 11:33:44 -0800

    added bookings frontend

---
+++ frontend/src/Components/Flights/Bookings.js
@@ -0,0 +1,60 @@
+import React from "react";
+import BookingsItem from "./BookingsItem";
+const Bookings = () => {
+  return (
+    <div className="past-flights">
+      <div className="transform endow rectangle">
+        <div className="swap">
+          <div className="transform endow rectangle card card--full my-account-past-flights-card">
+            <div className="swap">
+              <div className="header-bar">
+                <div className="transform endow rectangle header-bar--container">
+                  <div className="endow--backdrop-outer-container">
+                    <div className="endow--backdrop-background swa-g-color-bg-neutral-gray2"></div>
+                  </div>
+                  <div className="swap header-bar--content">
+                    <div className="header-bar--content">
+                      <h2
+                        className="heading heading_semi-large header-bar--heading header-bar--heading_focusable"
+                        id="past"
+                        tabindex="-1"
+                      >
+                        0 past flights
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="transform endow rectangle">
+                <div className="swap">
+                  <div className="my-account-past-flights-card--filter-area"></div>
+                  <div className="past-flights-card--no-results-section">
+                    <div className="my-account-no-results-content">
+                      {/* <span className="my-account-no-results-content--title">
+                        You have no past flights.
+                      </span> */}
+
+                      <div className="table-wrapper">
+                        <div class="table">
+                          <div class="row header">
+                            <div class="cell">Trip</div>
+                            <div class="cell">Date</div>
+                            <div class="cell">Traveller</div>
+                            <div class="cell">Location</div>
+                          </div>
+                          <BookingsItem />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bookings;
---
+++ frontend/src/Components/Flights/BookingsItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BookingList2 = () => {
+  return (
+    <div class="row">
+      <div class="cell" data-title="Name">
+        San Francisco
+      </div>
+      <div class="cell" data-title="Age">
+        Aug 09 2021
+      </div>
+      <div class="cell" data-title="Occupation">
+        John Doe
+      </div>
+      <div class="cell" data-title="Location">
+        BOM - SFO
+      </div>
+    </div>
+  );
+};
+
+export default BookingList2;
