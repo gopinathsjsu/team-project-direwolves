a87898b4f2f37a2ae4b859b90a98eae547677087
vinay guda <vinayguda05@gmail.com>
2021-11-30 01:37:37 -0800

    CreateReservation: Seat Selectio : plane design

--- backend/model/Booking.js
+++ backend/model/Booking.js
@@ -6,12 +6,10 @@ const BookingSchema = new mongoose.Schema(
         userId: { type: Schema.Types.ObjectId, ref: "UserProfile" },
         flightId: { type: Schema.Types.ObjectId, ref: "Flight" },
         bookingDate: { type: Date, default: Date.now() },
-        seatId: { type: Schema.Types.ObjectId, ref: "Seat" },
-        bookingStatus: {
-            type: String
-        },
+        seatNumber: { type: String },
+        bookingStatus: { type: String },
         price:{type:Number},
-        paidbyMileage:{type:Boolean, default:false},
+        isMileage:{type:Boolean, default:false},
     }
 );
 
