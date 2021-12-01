d5b8c5c31966be4f4fbaed94f1eb26b5b5a3b650
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-12-01 10:41:27 -0800

    Pushed API changes

--- backend/index.js
+++ backend/index.js
@@ -215,21 +215,26 @@ app.post("/getSeatInfoFromBookings", async (req, res, next) => {
 });
 
 
-app.post("/getbookings", async (req, res, next) => {
+app.post("/getBooking", async (req, res, next) => {
   try {
-    const bookings = [];
-    if (req.body.isadmin == false){ //adding conditions for isAdmin 
-     bookings = await Booking.find({ userId: req.body.userId })
-      .populate("userId", ["firstName", "lastName"])
-      .populate("flightId", ["departureAirport", "arrivalAirport", "arrivalDateTime", "departureDateTime"])
+    const bookings = await Booking.find()
+      .populate("userId")
+      .populate({
+        path: "flightId",
+        model: "Flight",
+        populate: [
+          {
+            path: "arrivalAirport",
+            model: "Airport",
+          },
+          {
+            path: "departureAirport",
+            model: "Airport",
+          },
+        ],
+      })
       .sort({ Time: "desc" });
-    }
-    else{
-      bookings = await Booking.find() 
-      .populate("userId", ["firstName", "lastName"])
-      .populate("flightId", ["departureAirport", "arrivalAirport", "arrivalDateTime", "departureDateTime"])
-      .sort({ Time: "desc" });
-    }
+
     return res.status(200).json({
       success: true,
       count: bookings.length,
@@ -242,7 +247,6 @@ app.post("/getbookings", async (req, res, next) => {
     });
   }
 });
-
 
 app.get("/allAirports", function (req, res) {
   Airport.find().exec((error, result) => {
