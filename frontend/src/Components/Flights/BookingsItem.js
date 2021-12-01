4be4fd8c2adf9f7b4ffb82437f9cb338682bfb1d
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-12-01 02:07:43 -0800

    Added frontend + backend of bookings

--- frontend/src/Components/Flights/BookingsItem.js
+++ frontend/src/Components/Flights/BookingsItem.js
@@ -5,16 +5,16 @@ const BookingList2 = ({booking}) => {
   return (
     <div class="brow">
       <div class="bcell" data-title="Name">
-        San Francisco
+        {booking.flightId.arrivalAirport.city}
       </div>
       <div class="bcell" data-title="Age">
-        Aug 09 2021
+        {booking.flightId.arrivalDateTime}
       </div>
       <div class="bcell" data-title="Occupation">
-        John Doe
+        {booking.userId.firstName} {booking.userId.lastName}
       </div>
       <div class="bcell" data-title="Location">
-        BOM - SFO
+      {booking.flightId.departureAirport.shortCode} - {booking.flightId.arrivalAirport.shortCode}
       </div>
     </div>
   );
