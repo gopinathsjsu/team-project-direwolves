56f48e5e58b9c1f65438313382d124792fd8f4a2
vinay guda <vinayguda05@gmail.com>
2021-12-02 21:15:42 -0800

    alignment

--- frontend/src/Components/MileageAccount/ManageMileage.js
+++ frontend/src/Components/MileageAccount/ManageMileage.js
@@ -5,9 +5,9 @@ import {
   Input,
   Col
 } from "reactstrap";
-import { Container,Form, FormControl,Button, Row} from "react-bootstrap";
+import { Container, Form, FormControl, Button, Row } from "react-bootstrap";
 import axios from "axios";
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { formatAMPM } from "./../Services/ControllerUtils";
 import NavigationBar from "../Navbar/Navbar";
@@ -17,20 +17,20 @@ import ProfilePic from "../svg/profilePic.png";
 class ManageMileage extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        userInfo: {
-          _id:"",
-          firstName:"",
-          lastName:"",
-          email:"",
-          address:"",
-          mileagePoint:0,
-          isAdmin:false,
-        },
-        mileageActivity:[],
-        error:"",
-        isSuccess: false,
-      };
+    this.state = {
+      userInfo: {
+        _id: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        address: "",
+        mileagePoint: 0,
+        isAdmin: false,
+      },
+      mileageActivity: [],
+      error: "",
+      isSuccess: false,
+    };
   }
 
   handleChange = e => {
@@ -53,54 +53,54 @@ class ManageMileage extends Component {
     //prevent page from refresh
     e.preventDefault();
     axios.defaults.withCredentials = true;
-      //make a post request with the user data
-      axios
-        .post(`http://localhost:3001/updateProfile`, this.state.userInfo)
-        .then((response) => {
-          console.log("Status Code : ", response.status);
-          if (response.status === 200) {
-            this.SetLocalStorage(JSON.stringify(this.state.userInfo));
-            this.setState({
-              isSuccess: true,
-              loginError:""
-            });
-          } else {
-            this.setState({
-              loginError: "User is already registered",
-              authFlag: false,
-              error: {},
-            });
-          }
-        })
-        .catch(() => {
+    //make a post request with the user data
+    axios
+      .post(`http://localhost:3001/updateProfile`, this.state.userInfo)
+      .then((response) => {
+        console.log("Status Code : ", response.status);
+        if (response.status === 200) {
+          this.SetLocalStorage(JSON.stringify(this.state.userInfo));
+          this.setState({
+            isSuccess: true,
+            loginError: ""
+          });
+        } else {
           this.setState({
             loginError: "User is already registered",
             authFlag: false,
+            error: {},
           });
+        }
+      })
+      .catch(() => {
+        this.setState({
+          loginError: "User is already registered",
+          authFlag: false,
         });
-    
+      });
+
   };
 
 
 
   //// 
-  getMileageActivity(){
-    axios.get(`http://localhost:3001/getMileageActivity?userId=${this.state.userInfo._id}`).then(response=>{
-        if(response.status==200){
-            console.log(response.data);
-            this.setState({
-              mileageActivity:response.data.data
-            })
-            console.log(this.state.mileageActivity);
-        }else{
-            this.setState({
-                error:response.error
-            })
-        }
-    }).catch(()=>{
+  getMileageActivity() {
+    axios.get(`http://localhost:3001/getMileageActivity?userId=${this.state.userInfo._id}`).then(response => {
+      if (response.status == 200) {
+        console.log(response.data);
         this.setState({
-            error:"Error while getting mileage points"
+          mileageActivity: response.data.data
         })
+        console.log(this.state.mileageActivity);
+      } else {
+        this.setState({
+          error: response.error
+        })
+      }
+    }).catch(() => {
+      this.setState({
+        error: "Error while getting mileage points"
+      })
     })
   }
 
@@ -108,7 +108,7 @@ class ManageMileage extends Component {
     if (typeof Storage !== "undefined") {
       if (localStorage.key("userData")) {
         this.setState({
-            userInfo: Object.assign(
+          userInfo: Object.assign(
             this.state.userInfo,
             JSON.parse(localStorage.getItem("userData"))
           )
@@ -130,10 +130,10 @@ class ManageMileage extends Component {
 
   render() {
     if (this.state.isSuccess) {
-     alert("Your profile has been updated successfully");
+      alert("Your profile has been updated successfully");
     }
 
-    if(!localStorage.key("userData")){
+    if (!localStorage.key("userData")) {
       return <Redirect to='/login' />
     }
 
@@ -146,17 +146,17 @@ class ManageMileage extends Component {
         )}
         <div className="container-fluid form-cont">
           <div className="flex-container">
-            <div className="row" style={{paddingTop:"80px", paddingBottom:"40px"}}>
-             {/* <div className="col-sm-2"></div> Hi {this.state.userInfo.firstName} */}
-             <div className="col-sm-1"></div>
-             <div className="col-sm-6" style={{paddingLeft:"50px"}}><h3>Manage Mileage Rewards</h3></div>
-             </div>
+            <div className="row" style={{ paddingTop: "80px", paddingBottom: "40px" }}>
+              {/* <div className="col-sm-2"></div> Hi {this.state.userInfo.firstName} */}
+              <div className="col-sm-1"></div>
+              <div className="col-sm-6" style={{ paddingLeft: "50px" }}><h3>Manage Mileage Rewards</h3></div>
+            </div>
             <div className="row">
               <div className="col-sm-1"></div>
               <div className="col col-sm-2">
-                <img src={ProfilePic} width="300" height="300" style={{paddingLeft:"50px"}}></img>
+                <img src={ProfilePic} width="300" height="300" style={{ padding: "30px" }}></img>
               </div>
-              <div className="col col-sm-6" style={{paddingLeft:"50px"}}>
+              <div className="col col-sm-6" style={{ paddingLeft: "50px", marginLeft: "50px" }}>
                 <div
                   id="errorLogin"
                   hidden={this.state.error.length > 0 ? false : true}
@@ -165,84 +165,93 @@ class ManageMileage extends Component {
                 >
                   {this.state.error}
                 </div>
-                <Form onSubmit={this.handleSubmit} className="form-stacked" style={{paddingLeft:"30px"}}>
-                   <Row>
-                       <Col>
-                        <FormGroup>
-                            <Label for="firstname">
-                            FirstName
-                            </Label>
-                            <Input
-                            type="text"
-                            id="name"
-                            name="firstName"
-                            placeholder="First Name"
-                            onChange={this.handleChange}
-                            value={this.state.userInfo.firstName}
-                            required
-                            ></Input>
-                        </FormGroup>
-                       </Col>
-                       <Col>
-                       <FormGroup>
-                            <Label for="lastname">
-                            LastName
-                            </Label>
-                            <Input
-                            type="text"
-                            id="lastName"
-                            name="lastName"
-                            placeholder="Last Name"
-                            value={this.state.userInfo.lastName}
-                            onChange={this.handleChange}
-                            required
-                            ></Input>
-                        </FormGroup>
-                       </Col>
-                   </Row>
-                  <FormGroup>
-                    <Label htmlFor="email">
-                      Here&apos;s my <strong>email address</strong>
-                    </Label>
-                    <Input
-                      data-testid="email-input-box"
-                      type="email"
-                      id="email"
-                      name="email"
-                      placeholder="Email"
-                      value={this.state.userInfo.email}
-                      onChange={this.handleChange}
-                      required
-                    ></Input>
-                  </FormGroup>
-                  <FormGroup>
-                    <Label htmlFor="address">
-                      Address
-                    </Label>
-                    <Input
-                      type="text"
-                      id="address"
-                      name="address"
-                      placeholder="Address"
-                      value={this.state.userInfo.address}
-                      onChange={this.handleChange}
-                    ></Input>
-                  </FormGroup>
+                <Form onSubmit={this.handleSubmit} className="form-stacked" style={{ paddingLeft: "30px" }}>
+                  <Row>
+                    <Col>
+                      <FormGroup>
+                        <Label for="firstname">
+                          FirstName
+                        </Label>
+                        <Input
+                          type="text"
+                          id="name"
+                          name="firstName"
+                          placeholder="First Name"
+                          onChange={this.handleChange}
+                          value={this.state.userInfo.firstName}
+                          required
+                        ></Input>
+                      </FormGroup>
+                    </Col>
+                    <Col>
+                      <FormGroup>
+                        <Label for="lastname">
+                          LastName
+                        </Label>
+                        <Input
+                          type="text"
+                          id="lastName"
+                          name="lastName"
+                          placeholder="Last Name"
+                          value={this.state.userInfo.lastName}
+                          onChange={this.handleChange}
+                          required
+                        ></Input>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col>
+                      <FormGroup>
+                        <Label htmlFor="email">
+                          Here&apos;s my <strong>email address</strong>
+                        </Label>
+                        <Input
+                          data-testid="email-input-box"
+                          type="email"
+                          id="email"
+                          name="email"
+                          placeholder="Email"
+                          value={this.state.userInfo.email}
+                          onChange={this.handleChange}
+                          required
+                        ></Input>
+                      </FormGroup>
+                    </Col>
+                    <Col>
+                      <FormGroup>
+                        <Label htmlFor="address">
+                          Address
+                        </Label>
+                        <Input
+                          type="text"
+                          id="address"
+                          name="address"
+                          placeholder="Address"
+                          value={this.state.userInfo.address}
+                          onChange={this.handleChange}
+                        ></Input>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+
                   <FormGroup>
                     <Label htmlFor="mileage">
-                    <strong>Current Mileage Points:</strong>
+                      <strong>Current Mileage Points:</strong>
                     </Label>
-                    <Label htmlFor="points" style={{paddingLeft:"5px"}}>
-                    {this.state.userInfo.mileagePoint}
+                    <Label htmlFor="points" style={{ paddingLeft: "5px" }}>
+                      {this.state.userInfo.mileagePoint}
                     </Label>
-                    </FormGroup>
+                  </FormGroup>
                   <FormGroup row>
                     <Col>
                       <Button
                         type="submit"
                         color="btn btn-primary"
                       >
-                       Update
+                        Update
                       </Button>
                     </Col>
                   </FormGroup>
@@ -250,60 +259,60 @@ class ManageMileage extends Component {
               </div>
             </div>
             <Container style={{ padding: "0 50px" }}>
-            {this.state.mileageActivity &&
-             this.state.mileageActivity.map((item,idx) => (
-              <Container
-                style={{
-                  padding: "20px",
-                  border: "1px solid #ddd",
-                  borderRadius: "15px",
-                }}
-                className="flight"
-              >
-                <Row>
-                  <Col md={9}>
-                    <Row className="item">
-                      <div>
-                        #
-                        <span style={{ fontWeight: "bold" }}>
-                          {item.flightId.name} {item.flightId.number}
-                        </span>
-                      </div>
-                    </Row>
-                    <Row className="item">
-                      <Col>
-                        {item.flightId.departureAirport.name},{" "}
-                        {item.flightId.departureAirport.city}
-                        <span style={{ margin: "0 24px" }}>To</span>
-                        {item.flightId.arrivalAirport.name}, {item.flightId.arrivalAirport.city}
+              {this.state.mileageActivity &&
+                this.state.mileageActivity.map((item, idx) => (
+                  <Container
+                    style={{
+                      padding: "20px",
+                      border: "1px solid #ddd",
+                      borderRadius: "15px",
+                    }}
+                    className="flight"
+                  >
+                    <Row>
+                      <Col md={9}>
+                        <Row className="item">
+                          <div>
+                            #
+                            <span style={{ fontWeight: "bold" }}>
+                              {item.flightId.name} {item.flightId.number}
+                            </span>
+                          </div>
+                        </Row>
+                        <Row className="item">
+                          <Col>
+                            {item.flightId.departureAirport.name},{" "}
+                            {item.flightId.departureAirport.city}
+                            <span style={{ margin: "0 24px" }}>To</span>
+                            {item.flightId.arrivalAirport.name}, {item.flightId.arrivalAirport.city}
+                          </Col>
+                        </Row>
+                        <Row className="item" style={{ width: "85%" }}>
+                          <Col className="time">
+                            <div>Depart at </div>
+                            <div>{formatAMPM(item.flightId.departureDateTime)}</div>
+                          </Col>
+                          <Col className="time">
+                            <div>Arrives at </div>
+                            <div>{formatAMPM(item.flightId.arrivalDateTime)}</div>
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col md={3} className="right">
+                        <div style={{ textAlign: "center" }}>
+                          Price : {item.price}<br />
+                          MileagePoint:{item.isMileage ? <span style={{ color: "red" }}>-{item.price}</span> : <span style={{ color: "green" }}>+{Math.round(item.price * 0.1)}</span>}
+                        </div>
+                        <div>
+                        </div>
                       </Col>
                     </Row>
-                    <Row className="item" style={{ width: "85%" }}>
-                      <Col className="time">
-                        <div>Depart at </div>
-                        <div>{formatAMPM(item.flightId.departureDateTime)}</div>
-                      </Col>
-                      <Col className="time">
-                        <div>Arrives at </div>
-                        <div>{formatAMPM(item.flightId.arrivalDateTime)}</div>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col md={3} className="right">
-                    <div style={{ textAlign: "center" }}>
-                      Price : {item.price}<br/>
-                      MileagePoint:{item.isMileage?<span style={{color:"red"}}>-{item.price}</span>:<span style={{color:"green"}}>+{Math.round(item.price*0.1)}</span>}
-                    </div>
-                    <div>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            ))}
+                  </Container>
+                ))}
             </Container>
           </div>
         </div>
-        </>
+      </>
     );
   }
 }
